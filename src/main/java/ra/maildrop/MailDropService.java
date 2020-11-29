e4223225da0c82448e0c636ccd7804a14a48eed3
objectorange <brian@resolvingarchitecture.io>
2020-11-29 13:49:57 -0500

    completed first implementation of mail drop - tested

--- src/main/java/ra/maildrop/MailDropService.java
+++ src/main/java/ra/maildrop/MailDropService.java
@@ -7,7 +7,10 @@ import ra.common.service.BaseService;
 import ra.common.service.ServiceStatus;
 import ra.common.service.ServiceStatusObserver;
 import ra.util.Config;
+import ra.util.FileUtil;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 import java.util.logging.Logger;
 
@@ -25,6 +28,7 @@ public class MailDropService extends BaseService {
     public static final String RA_MAIL_DROP_DIR = "ra.maildrop.dir";
 
     protected Properties config;
+    protected File mailBoxDirectory;
 
     public MailDropService(MessageProducer producer, ServiceStatusObserver observer) {
         super(producer, observer);
@@ -43,13 +47,49 @@ public class MailDropService extends BaseService {
     @Override
     public boolean send(Envelope e) {
         // Persist message into mail box
-
+        File envFile = new File(mailBoxDirectory, e.getId());
+        try {
+            if(!envFile.createNewFile()) {
+                LOG.warning("Unable to create file to persist Envelope waiting on network");
+                return false;
+            }
+        } catch (IOException ioException) {
+            LOG.warning(ioException.getLocalizedMessage());
+            return false;
+        }
+        FileUtil.writeFile(e.toJSON().getBytes(), envFile.getAbsolutePath());
+        LOG.info("Persisted envelope (id="+e.getId()+") to file.");
         return true;
     }
 
-    private void pickUp(Envelope e) {
+    private boolean pickUp(Envelope e) {
         // Pickup messages for peer/client
-
+        boolean remove = false;
+        if(e.getValue("ra.maildrop.remove")!=null && "true".equals(e.getValue("ra.maildrop.remove"))) {
+            remove = true;
+        }
+        File mail = new File(mailBoxDirectory, e.getId());
+        if(!mail.exists()) {
+            e.addErrorMessage("Envelope does not exist.");
+            return true;
+        }
+        if(!mail.canRead()) {
+            e.addErrorMessage("Unable to read persisted envelope.");
+            return true;
+        }
+        byte[] bytes;
+        try {
+            bytes = FileUtil.readFile(mail.getAbsolutePath());
+        } catch (IOException ex) {
+            LOG.warning(ex.getLocalizedMessage());
+            return false;
+        }
+        String json = new String(bytes);
+        e.fromJSON(json);
+        if(remove && !mail.delete()) {
+            e.addErrorMessage("Unable to delete persisted envelope.");
+        }
+        return true;
     }
 
     @Override
@@ -64,6 +104,11 @@ public class MailDropService extends BaseService {
             return false;
         }
         config.put(RA_MAIL_DROP_DIR, getServiceDirectory().getAbsolutePath());
+        mailBoxDirectory = new File(getServiceDirectory(), "msg");
+        if(!mailBoxDirectory.exists() && !mailBoxDirectory.mkdir()) {
+            LOG.severe("Unable to create message hold directory.");
+            return false;
+        }
         updateStatus(ServiceStatus.STARTING);
         return true;
     }
--- src/test/java/ra/maildrop/MailDropServiceTest.java
+++ src/test/java/ra/maildrop/MailDropServiceTest.java
@@ -8,6 +8,7 @@ import ra.common.DLC;
 import ra.common.Envelope;
 import ra.common.network.NetworkBuilderStrategy;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
@@ -39,13 +40,28 @@ public class MailDropServiceTest {
         service.gracefulShutdown();
     }
 
-//    @Test
-//    public void sendTest() {
-//
-//    }
-//
-//    @Test
-//    public void pickUpTest() {
-//
-//    }
+    @Test
+    public void sendAndPickupTest() {
+        Envelope eSend = Envelope.documentFactory();
+        String id = eSend.getId();
+        eSend.addRoute(MailDropService.class, MailDropService.OPERATION_SEND);
+        eSend.addNVP("test","1234");
+        service.handleDocument(eSend);
+        Assert.assertTrue(new File(service.mailBoxDirectory,id).exists());
+
+        Envelope ePickup = Envelope.documentFactory(id);
+        ePickup.addRoute(MailDropService.class, MailDropService.OPERATION_PICKUP);
+        service.handleDocument(ePickup);
+        Assert.assertTrue(new File(service.mailBoxDirectory,id).exists());
+        Assert.assertTrue("1234".equals(ePickup.getValue("test")));
+
+        Envelope ePickupRemove = Envelope.documentFactory(id);
+        ePickupRemove.addRoute(MailDropService.class, MailDropService.OPERATION_PICKUP);
+        ePickupRemove.addNVP("ra.maildrop.remove","true");
+        service.handleDocument(ePickupRemove);
+        Assert.assertTrue("1234".equals(ePickup.getValue("test")));
+        Assert.assertFalse(new File(service.mailBoxDirectory,id).exists());
+
+    }
+
 }
